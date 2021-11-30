da41deb4ce5a3fcf3e9810278e529c095cba0b21
Omar I. Martínez Gómez <omar_238@hotmail.com>
2021-11-30 17:58:14 -0600

    ejercicio de establecer oculto incompleto del libro

--- libro/cap05/formularioSimple.php
+++ libro/cap05/formularioSimple.php
@@ -1,2 +1,7 @@
 <?php
 include_once "_constantes.php";
+
+echo "El nombre tecleado es: " . $_POST['nombre'] . BR;
+echo "El estado soltero es: " . $_POST['soltero'] . BR;
+echo "El rango de edad es: " . $_POST['edad'] . BR;
+echo "El campo oculto vale: " . $_POST['datoOculto'] . BR;
