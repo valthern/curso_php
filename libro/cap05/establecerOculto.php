4f810593e71c41af45699da303a1032c810ee87d
Omar I. Martínez Gómez <omar_238@hotmail.com>
2021-12-05 22:45:28 -0600

    libro -> trabajo con variables de matrices $_GET y $_POST completada

---
+++ libro/cap05/establecerOculto.php
@@ -0,0 +1,5 @@
+<?php
+include_once "_constantes.php";
+
+echo "El valor del campo oculto es: " . $_POST["ocultoSoltero"] . BR;
+//echo "El valor del campo oculto es: " . $HTTP_POST_VARS["ocultoSoltero"] . BR;
