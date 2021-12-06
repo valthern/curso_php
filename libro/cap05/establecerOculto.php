89c292b11249ab10bf94879d34273bba934ae54f
Omar I. Martínez Gómez <omar_238@hotmail.com>
2021-12-06 00:36:43 -0600

    udemy1 -> .htaccess completo y funcionando

--- libro/cap05/establecerOculto.php
+++ libro/cap05/establecerOculto.php
@@ -2,4 +2,6 @@
 include_once "_constantes.php";
 
 echo "El valor del campo oculto es: " . $_POST["ocultoSoltero"] . BR;
+// Este forma de la matriz parece ya no estar disponible en la versión 8
+// de Php
 //echo "El valor del campo oculto es: " . $HTTP_POST_VARS["ocultoSoltero"] . BR;
