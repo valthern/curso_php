956c546a2e67aaa9fe3ba35716fe398b50e16a7d
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-01-12 22:52:38 -0600

    libro -> averiguar donde se guardan los archivos

--- libro/cap00/index.php
+++ libro/cap00/index.php
@@ -38,12 +38,10 @@ echo BR . BR;
 
 $ciudades = array("Madrid", "Barcelona", "Londres", "New York", "Los Angeles", "Chicago");
 foreach ($ciudades as $clave => $valor) {
-	echo "La ciudad con el índice $clave tiene el nombre $valor.".BR;
+	echo "La ciudad con el índice $clave tiene el nombre $valor." . BR;
 }
 echo BR;
 $ciudades = array("md" => "Madrid", "ba" => "Barcelona", "lo" => "Londres", "ny" => "New York", "la" => "Los Angeles", "ch" => "Chicago");
 foreach ($ciudades as $clave => $valor) {
-	echo "La ciudad con el índice $clave tiene el nombre $valor.".BR;
+	echo "La ciudad con el índice $clave tiene el nombre $valor." . BR;
 }
-
-?>
