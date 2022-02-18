f17ff6600ae21e204b1de078428395312a7cecda
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-02-17 22:26:30 -0600

    libro -> comprobarFichero.php completo

--- desarrolloweb/i.php
+++
@@ -1,2 +0,0 @@
-<?php echo php_ini_loaded_file(); ?>
-/* https://code.tutsplus.com/es/tutorials/how-to-upload-a-file-in-php-with-example--cms-31763 */
---
+++ libro/cap05/comprobarFichero.php
@@ -0,0 +1,21 @@
+<?php
+// Se comprueba si el archivo tiene el formato adecuado
+if ($_FILES["fichero"]["type"] != "image/pjpeg") {
+    die("El ficheero no tiene el formato adecuado");
+}
+// Se comprueba que el fichero tenga un peso adecuado
+if ($_FILES["fichero"]["size"] > 200000) {
+    die("El fichero es demasiado grande");
+}
+// Se comprueba que no se hayan producido errores
+if ($_FILES["fichero"]["error"] != 0) {
+    die("Se ha producido un error");
+}
+// Por último, se intenta grabar y se comprueba que se haya grabado bien
+$archivoRecibido = $_FILES["fichero"]["tmp_name"];
+$destino = "ficherosSubidos/fotoDelUsuario.jpg";
+if (move_uploaded_file($archivoRecibido, $destino)) {
+    echo "Fichero grabado exitosamente";
+} else {
+    echo "El fichero no se ha podido grabar";
+}
