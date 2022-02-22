289b8e84c4c6ab9777e87c5ab6d641762187aeba
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-02-22 16:04:16 -0600

    libro -> comprobarYVolver completado, pero con dudas

---
+++ libro/cap05/comprobarYVolver.php
@@ -0,0 +1,30 @@
+<?php
+include_once "_constantes.php";
+$error = false;
+
+// Se comprueba si el archivo tiene el formato adecuado
+if ($_FILES["fichero"]["type"] != "image/pjpeg") {
+    $error = true;
+}
+// Se comprueba que tenga un peso adecuado
+if ($_FILES["fichero"]["size"] > 200000) {
+    $error = true;
+}
+// Se comprueba que no se hayan producido errores
+if ($_FILES["fichero"]["error"] != 0) {
+    $error = true;
+}
+// Si se ha producido algún error se vuelve al formulario inicial
+if ($error) {
+    echo "<script type='text/javascript'>";
+    echo "location.href='comprobarYVolver.html'";
+    echo "</script>";
+}
+// Por último se intenta grabar y se comprueba si se graba bien
+$archivoRecibido = $_FILES["fichero"]["tmp_name"];
+$destino = "ficherosSubidos/fotoDelUsuario.jpg";
+if (move_uploaded_file($archivoRecibido, $destino)) {
+    echo "Fichero grabado";
+} else {
+    echo "El fichero no se ha podido grabar";
+}
