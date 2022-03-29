a523e9ae6347f9c76714558300fb69fd60f51c29
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-03-28 22:30:04 -0600

    libro -> todavía empezado ejercicio del sscanf

--- libro/cap06/ascii.php
+++ libro/cap06/ascii.php
@@ -4,4 +4,4 @@ include_once "_constantes.php";
 $caracter = 'A';
 $valor = 97;
 echo "El carácter '$caracter' (mayúscula) tiene el código ASCII: " . ord($caracter) . BR;
-echo "El valor $valor corresponde al caracter: " . chr($valor) . BR;
+echo "El valor $valor corresponde al caracter: '" . chr($valor) . "'" . BR;
---
+++ libro/cap06/valorFormateado.php
@@ -0,0 +1,7 @@
+<?php
+include_once "_constantes.php";
+
+$precio1=68.75;
+$precio2=54.35;
+$precio=$precio1+$precio2;
+echo "El valor de \$precio es $precio".BR;
