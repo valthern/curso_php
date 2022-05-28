77441ecce09cdce0ade3f43a9a9cd14b84e441d6
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-05-27 22:36:10 -0500

    regex -> curso completo

--- regex/index.js
+++ regex/index.js
@@ -1,8 +1,18 @@
-const regex = /^pato1?$/;
-const text = "pato1";
+const regex = /Java\s(?!8|9)/
+const text = "Java "
+
 
 if (regex.test(text)) {
   console.log("Es correcto");
 } else {
   console.error("Es Incorrecto");
 }
+
+
+//const regex = /<beer>.*?<\/beer>/g
+//const text = "<beer>corona</beer><beer>heineken</beer>"
+//console.log(text.match(regex))
+
+// const regex = /(Vino|Cerveza)(=?\s\w+)/g
+// const text = "Vino tinto Vino blanco Vino feo Cerveza IPA"
+// console.log(text.match(regex))
