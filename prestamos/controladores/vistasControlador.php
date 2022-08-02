315a4185279a203d9fc7882375d18cf2bbd9f5d2
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-08-02 00:46:31 -0500

    udemy -> 2.5 Modelo y controlador vistas 25:11

--- prestamos/controladores/vistasControlador.php
+++ prestamos/controladores/vistasControlador.php
@@ -1,2 +1,20 @@
 <?php
-	
+require_once "./modelos/vistasModelo.php";
+
+class vistasControlador extends vistasModelo{
+    /*---------- Controlador obtener plantilla  ----------*/
+    public function obtener_plantilla_controlador(){
+        return require_once "./vistas/plantilla.php";
+    }
+
+    /*---------- Controlador obtener plantilla  ----------*/
+    public function obtener_vistas_controladores(){
+        if (isset($_GET['views'])) {
+            $ruta=explode("/", $_GET['views']);
+            $respuesta=vistasModelo::obtener_vistas_modelo($ruta[0]);
+        } else {
+            $respuesta="login";
+        }
+        return $respuesta;
+    }
+}
