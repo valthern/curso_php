407e39830e5c10be85440d86a98465b21245af26
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-08-10 23:23:19 -0500

    prestamos -> otra clase

--- prestamos/modelos/vistasModelo.php
+++ prestamos/modelos/vistasModelo.php
@@ -5,6 +5,7 @@ class vistasModelo
 	protected static function obtener_vistas_modelo($vistas)
 	{
 		$listaBlanca = [];
+		
 		if (in_array($vistas, $listaBlanca)) {
 			if (is_file("./vistas/contenidos/" . $vistas . "-view.php")) {
 				$contenido = "./vistas/contenidos/" . $vistas . "-view.php";
