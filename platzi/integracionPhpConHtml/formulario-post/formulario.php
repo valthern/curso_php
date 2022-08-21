f7c990109d672569e5eb7a0453eab796aed29fc8
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-08-21 01:22:49 -0500

    platzi -> clase 12 comienzo

--- platzi/integracionPhpConHtml/formulario-post/formulario.php
+++ platzi/integracionPhpConHtml/formulario-post/formulario.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario por POST</title>
+    <title>Formulario por GET</title>
 </head>
 <body>
-    <form action="./server.php" method="post">
+    <form action="./server.php" method="get">
         <label for="nombre">Nombre:</label>
         <input type="text" name="nombre" id="nombre">
         <label for="edad">Edad:</label>
