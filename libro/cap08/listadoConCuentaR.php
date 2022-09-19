587e280f4af32c34e59bc47d7e3e5a53340eb645
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-09-18 23:32:10 -0500

    libro -> adición de listado con cuenta.php

---
+++ libro/cap08/listadoConCuentaR.php
@@ -0,0 +1,12 @@
+<?php
+include_once "_constantes.php";
+
+$manejadorFichero1 = fopen("lecturaSimple.txt", "r");
+echo "El contenido del fichero es: <b>";
+// Se muestra el contenido del fichero y se asigna el total de caracteres 
+// a una variable
+$caracteres = fpassthru($manejadorFichero1);
+echo "</b>" . BR . "El total de caracteres es de: <b>";
+// Se muestra el total de caracteres
+echo $caracteres . "</b>" . BR;
+fclose($manejadorFichero1);
