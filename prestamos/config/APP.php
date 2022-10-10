dc9278dfcca5164e05ea76a662e3a05a3d9b0f35
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-10-09 23:29:35 -0500

    udemy -> adicion de app.php y server.php

--- prestamos/config/APP.php
+++ prestamos/config/APP.php
@@ -1,6 +1,6 @@
 <?php
-//const SERVERURL="http://192.168.64.2/curso_php/prestamos/";
-const SERVERURL = "http://localhost/curso_php/prestamos/";
+const SERVERURL="http://192.168.64.2/curso_php/prestamos/";
+// const SERVERURL = "http://localhost/curso_php/prestamos/";
 const COMPANY = "SISTEMAS PRESTAMOS";
 const MONEDA = "$";
 date_default_timezone_set("America/Mexico_City");
--- prestamos/config/SERVER.php
+++ prestamos/config/SERVER.php
@@ -1,11 +1,11 @@
 <?php
-	const SEVER="localhost";
-	const DB="prestamos";
-	const USER="root";
-	const PASS="";
+const SEVER = "localhost";
+const DB = "prestamos";
+const USER = "root";
+const PASS = "";
 
-	const SGBD="mysql:host=".SEVER.";dbname=".DB;
+const SGBD = "mysql:host=" . SEVER . ";dbname=" . DB;
 
-	const METHOD="AES-256-CBC";
-	const SECRET_KEY='$PRESTAMOS@2020';
-	const SECRET_IV='037970';
+const METHOD = "AES-256-CBC";
+const SECRET_KEY = '$PRESTAMOS@2020';
+const SECRET_IV = '037970';
